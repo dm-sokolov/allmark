d43c4266983d1ea108be96053aaf7da2d7cfd275
Николай Есаулков <nikolay_e@holdingbp.ru>
2019-10-12 14:13:23 +0500

    Пока не надо

--- AllMark/AllMark/Controllers/HomeController.cs
+++ AllMark/AllMark/Controllers/HomeController.cs
@@ -2,24 +2,15 @@
 using Microsoft.AspNetCore.Mvc;
 using AllMark.Models;
 using Microsoft.AspNetCore.Authorization;
-using AllMark.Services.Interfaces;
 using System.Threading.Tasks;
 
 namespace AllMark.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly IEmailService _emailService;
-
-        public HomeController(IEmailService emailService)
-        {
-            _emailService = emailService;
-        }
-
         [Authorize]
         public async Task<IActionResult> Index()
         {
-            await _emailService.SendEmailAsync("esaulkovNikolay@yandex.ru", "test subject", "test message");
             return View();
         }
 
