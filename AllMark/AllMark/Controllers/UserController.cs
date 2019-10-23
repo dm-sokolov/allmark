f35045aa5dcc35184be9ec5ff6ca581ab22d4428
Николай Есаулков <nikolay_e@holdingbp.ru>
2019-10-23 19:04:45 +0500

    -

---
+++ AllMark/AllMark/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using AllMark.Core.Models;
+using AllMark.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AllMark.Controllers
+{
+    public class UserController: Controller
+    {
+        private readonly IRepository<User> _userRepository;
+
+        public UserController(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<ActionResult> Update(User userModel)
+        {
+            var user = await _userRepository.GetByIdAsync(userModel?.Id);
+            user.Name = userModel.Name;
+            await _userRepository.UpdateAsync(user);
+            return Json(user);
+        }
+    }
+}
