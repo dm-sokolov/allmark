6482a9324b9b7e346017e1c548a804d76b9cfa6f
Dmitriy Sokolov <dmitirii_s@holdingbp.ru>
2019-11-26 00:32:02 +0500

    ТЕСТ - вызовы методов из контроллера

--- AllMark/AllMark/Controllers/HomeController.cs
+++ AllMark/AllMark/Controllers/HomeController.cs
@@ -1,17 +1,47 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AllMark.Models;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using AllMark.Controllers.Base;
+using AllMark.Services.Interfaces;
+using FluentNHibernate.Testing.Values;
+using Utils.NationalCatalog.Models;
 
 namespace AllMark.Controllers
 {
     public class HomeController : BaseController
     {
-        [Authorize]
-        public IActionResult Index() => View();
+        private readonly INationalCatalogService _nationalCatalogService;
 
+        public HomeController(INationalCatalogService nationalCatalogService)
+        {
+            _nationalCatalogService = nationalCatalogService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Index() // => View();
+        {
+            var draftGtins = await _nationalCatalogService.GenerateGtins(1);
+
+            var brands = await _nationalCatalogService.GetBrands();
+
+            var feed = new List<CatalogFeed>
+            {
+                new CatalogFeed()
+                {
+                    GTIN = draftGtins.CatalogDrafts.FirstOrDefault()?.Gtin,
+                    GoodName = "Пробный товар",
+                    TNVED = "3303",
+                    Brand = brands.FirstOrDefault()?.Name
+                }
+            };
+            var feedResult = await _nationalCatalogService.Feed(feed);
+
+            return View();
+        }
         public IActionResult ForLegalEntity() => View();
 
         public IActionResult ForIP() => View();
