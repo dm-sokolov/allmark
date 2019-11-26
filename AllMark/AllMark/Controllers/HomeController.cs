adf26e00d2d01346d909700cebd64fa46d4d3555
Dmitriy Sokolov <dmitirii_s@holdingbp.ru>
2019-11-26 23:03:30 +0500

    комит с нашими запросами

--- AllMark/AllMark/Controllers/HomeController.cs
+++ AllMark/AllMark/Controllers/HomeController.cs
@@ -24,25 +24,32 @@ namespace AllMark.Controllers
         [Authorize]
         public async Task<IActionResult> Index() // => View();
         {
-            var draftGtins = await _nationalCatalogService.GenerateGtins(1);
+            //var draftGtins = await _nationalCatalogService.GenerateGtins(1);
 
-            var brands = await _nationalCatalogService.GetBrands();
+            //var brands = await _nationalCatalogService.GetBrands();
 
-            var feed = new List<CatalogFeed>
-            {
-                new CatalogFeed()
-                {
-                    GTIN = draftGtins.CatalogDrafts.FirstOrDefault()?.Gtin,
-                    GoodName = "Пробный товар",
-                    TNVED = "3303",
-                    Brand = brands.FirstOrDefault()?.Name
-                }
-            };
-            var feedResult = await _nationalCatalogService.Feed(feed);
+            //var feed = new List<CatalogFeed>
+            //{
+            //    new CatalogFeed()
+            //    {
+            //        GTIN = draftGtins.CatalogDrafts.FirstOrDefault()?.Gtin,
+            //        GoodName = "Пробный товар",
+            //        TNVED = "3303",
+            //        Brand = brands.FirstOrDefault()?.Name
+            //    }
+            //};
+            //var feedResult = await _nationalCatalogService.Feed(feed);
 
             return View();
         }
-        public IActionResult ForLegalEntity() => View();
+
+        public async Task<IActionResult> ForLegalEntity()
+        {
+            //var attr = await _nationalCatalogService.GetAttributes(30141);
+            //var query = string.Join("\n",attr.GroupBy(i => i.GroupName).Select(i => $"Group {i.Key} {i.Count()} \n {string.Join("\n",i.Select(k => k.Name).ToList())}\n\n"));
+            
+            return View();
+        }//=> View();
 
         public IActionResult ForIP() => View();
 
