950d4fdc3b66d024274f95a13864a197604a9436
Dmitriy Sokolov <dmitirii_s@holdingbp.ru>
2019-11-28 22:18:06 +0500

    попытки

--- AllMark/AllMark/Controllers/ProductController.cs
+++ AllMark/AllMark/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using Kendo.Mvc.UI;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AllMark.Controllers
