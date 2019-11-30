0de1bb577e7d22b625601103bac05ac19b6b94ed
Николай Есаулков <nikolay_e@holdingbp.ru>
2019-11-30 15:57:45 +0500

    Вроде фикс стилей

--- AllMark/AllMark/wwwroot/js/site.js
+++ AllMark/AllMark/wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(() => {
+    setControlsSettings();
+});
 
-// Write your JavaScript code.
+
+function setControlsSettings() {
+    // открытие выпадашки комобокса по нажатию на поле, а не только на "треугольник"
+    $('[data-role=combobox]').each(function() {
+        var widget = $(this).getKendoComboBox();
+        widget.input.focus(() => {
+            widget.open();
+            widget.input.select();
+        });
+        widget.input[0].setAttribute("autocomplete", "off");
+    });
+
+    $('[data-role=datepicker]').each(function () {
+        var widget = $(this).getKendoDatePicker();
+        $(widget.element).on('change', validateDateInput);
+    });
+
+    $('input[type=text]').focus(function() {
+        var input = $(this);
+        clearTimeout(input.data('selectTimeId'));
+        const selectTimeId = setTimeout(() => { input.select(); });
+        input.data('selectTimeId', selectTimeId);
+    }).blur(clearTimeout($(this).data('selectTimeId')));
+
+    $('input').focus(function() {
+        $(this).select();
+    });
+
+    $('input[data-role=numerictextbox]').focus(function () {
+        const input = $(this).select();
+        if (input.value === '0') {
+            input.value = '';
+        }
+    });
+}
+
