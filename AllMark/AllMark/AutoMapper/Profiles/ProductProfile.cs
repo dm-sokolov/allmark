d1919c40dcfad3baec868a40c5281c8d17d5b4eb
Dmitriy Sokolov <dmitirii_s@holdingbp.ru>
2019-12-08 22:09:38 +0500

    профиль

---
+++ AllMark/AllMark/AutoMapper/Profiles/ProductProfile.cs
@@ -0,0 +1,14 @@
+﻿using AllMark.Core.Models;
+using AllMark.DTO;
+using AutoMapper;
+
+namespace AllMark.AutoMapper.Profiles
+{
+    public class ProductProfile: Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<Product, ProductDto>();
+        }
+    }
+}
