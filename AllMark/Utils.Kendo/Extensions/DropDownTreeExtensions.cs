d189a6f06800d5060aa8e11a10a2a6f0d7365ef2
Николай Есаулков <nikolay_e@holdingbp.ru>
2019-12-14 20:44:04 +0500

    -

---
+++ AllMark/Utils.Kendo/Extensions/DropDownTreeExtensions.cs
@@ -0,0 +1,25 @@
+﻿using AllMark.DTO;
+using Kendo.Mvc.UI.Fluent;
+
+namespace Utils.Kendo.Extensions
+{
+    public static class DropDownTreeExtensions
+    {
+        public static DropDownTreeBuilder BaseDropDownTree(this DropDownTreeBuilder builder)
+        {
+            return builder.DataTextField(nameof(CategoryDto.Name))
+                        .DataValueField(nameof(CategoryDto.Id))
+                        .AutoWidth(true);
+        }
+
+        public static DropDownTreeBuilder CategoryDropDownTree(this DropDownTreeBuilder builder)
+        {
+            return builder.Placeholder("Категория 'по умолчанию'")
+                          .DataSource(i => i.Read(read => read.Action("GetCategories", "Product"))
+                                .Model(model => model.Id(nameof(CategoryDto.CategoryId))
+                                                     .HasChildren(nameof(CategoryDto.HasChildren))
+                                )
+                            );
+        }
+    }
+}
